0eddca4bbd9354a217fab1e63c74ea4b0650d2f4
Jota-Erre-JR <juliocesarjr@outlook.com>
2023-08-29 10:36:07 -0300

    Add Header component

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header-title">DS Sales</h1>
+    </header>
+  );
+}
+
+export default Header;
