a00e8944501c959e0d5b70d7a600c5fb972996f2
Jota-Erre-JR <juliocesarjr@outlook.com>
2023-09-05 11:46:26 -0300

    Add Filter component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
+import Filter from './components/filter';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="app-container">
+        <Filter />
+      </div>
     </>
   );
 }
